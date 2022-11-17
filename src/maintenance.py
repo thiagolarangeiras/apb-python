56ae29012b4bf1e4657e8b458ae59c2c94c905dd
jpdarabas <jpdarabas@gmail.com>
2022-11-17 16:44:09 -0300

    now register has mandatory data
    clientName, cpf, details and budget are now mandatory data.

--- src/maintenance.py
+++ src/maintenance.py
@@ -7,14 +7,26 @@ def register(maintenances)->int:
             newRegister = {}
             newRegister["idMaintenance"] = (maintenances[-1]["idMaintenance"] + 1) if (len(maintenances) > 0) else (1)  
             newRegister["clientName"] = input("Nome do titular: ") 
+            if newRegister["clientName"] == '':
+                raise Exception
             newRegister["cpf"] = input("CPF: ")
+            if newRegister["cpf"] == '':
+                raise Exception
             newRegister["veicleType"] =  EnumVeicleTypes(input("Tipo do veiculo(carro: C | Moto: M | Outro: O): ").upper())
             print("Detalhes do veiculo:")
             newRegister["details"] = {}
             newRegister["details"]["brand"] = input("\tMarca: ") 
+            if newRegister["details"]["brand"] == '':
+                raise Exception
             newRegister["details"]["model"] = input("\tModelo: ")
+            if newRegister["details"]["model"] == '':
+                raise Exception
             newRegister["details"]["color"] = input("\tCor: ")
+            if newRegister["details"]["color"] == '':
+                raise Exception
             newRegister["budget"] =  float(input("Orçamento: "))
+            if newRegister["budget"] == '':
+                raise Exception
             newRegister["description?"] = input("Descrição do serviço: ")
             newRegister["entryDate"] = datetime.now()
             newRegister["exitDate?"] = datetime.min
