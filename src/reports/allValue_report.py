9f7a5bd6c4a3704b15910ba7359f5f30dd8f8213
João Pedro Darabas <90244580+jpdarabas@users.noreply.github.com>
2022-12-08 02:52:46 -0300

    fixed get_allValue

--- src/reports/allValue_report.py
+++ src/reports/allValue_report.py
@@ -5,7 +5,7 @@ import pandas as pd
 def get_allValue():
     maintenances =  Maintenence.query.filter_by(status=str([EnumStatus.finished]))
     df_maintenances = pd.DataFrame(maintenances)
-    df_allValue['ID'] = df_maintenances['id']
-    df_allValue['Valor'] = df_maintenances['value']
-    print(df_allValue)
-    print("\nTotal:{}R$\n".format(sum(df_allValue['Valor'])))
+    df_maintenances.rename({'id': 'ID',
+                            'value': 'Valor'}, axis = 1, inplace = True)
+    print(df_maintenances[['ID', 'Valor']])
+    print("\nTotal:{}R$\n".format(sum(df_maintenances['Valor'])))
